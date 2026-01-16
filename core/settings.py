2331e4e80e4d842e54f5c4107e9cbdf44e6f91f7
Elton <macocaeltonmarciomacoca@email.com>
2026-01-16 03:22:17 +0200

    fix2: db on i hope

--- core/settings.py
+++ core/settings.py
@@ -102,11 +102,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Se não encontrar a variável DATABASE_URL (no seu PC), usa SQLite
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
